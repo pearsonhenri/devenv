5847423f587e954bdc722e6e1bdeda7534d128f1
Pearson Henri <pearsonhenri@gmail.com>
2018-10-15 21:31:54 -0700

    add startmeup.sh

---
+++ startmeup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# ensure XCode is installed (https://stackoverflow.com/questions/30533039/verify-xcode-command-line-tools-install-in-bash-script)
+check=$((xcode-\select --install) 2>&1)
+echo $check
+str="xcode-select: note: install requested for command line developer tools"
+while [[ "$check" == "$str" ]];
+do
+  osascript -e 'tell app "System Events" to display dialog "xcode command-line tools missing." buttons "OK" default button 1 with title "xcode command-line tools"'
+  exit;  
+done
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install python and its friends (courtesy of http://www.pyladies.com/blog/Get-Your-Mac-Ready-for-Python-Programming/)
+brew install python
+curl -O http://python-distribute.org/distribute_setup.py
+python distribute_setup.py
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+
+# install virtualenv
+pip install virtualenv
+
+# install git
+brew install git
+git config --global user.email "pearsonhenri@gmail.com"
+git config --global user.name "Pearson Henri"
